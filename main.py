b4a1750952bc8d6a79e8c6a7adab4243b5ec513d
apriLion <28001843+FawazQutami@users.noreply.github.com>
2020-05-28 00:13:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 import argparse
 import numpy as np
-import datetime as dt
-
-from GAAlgorithm import GeneticAlgorithm as GA \
-    , Population \
-    , TripManager \
-    , Node
-
-from eHandler import PrintException as EH
-from plotTSP import plotSA, plot_learning
-from setup import install_required_Packages
 
 
 def coordinatesFile(fileName):
@@ -46,11 +36,11 @@ def runGenetic(Nodes):
         ga = GA(Nodes
                 , mutationRate=0.05
                 , tournamentSize=15
-                , eliteSize=True)
+                , elite=True)
 
         fitnessResults = []
         nextGeneration = ga.matingPool(initialPopulation)
-        for i in range(0, 1000):
+        for i in range(0, 500):
             nextGeneration = ga.matingPool(nextGeneration)
             fitnessResults.append(nextGeneration.getFittest().getDistance())
 
@@ -75,57 +65,54 @@ def runGenetic(Nodes):
 def main():
     """ """
     try:
-        print()
-        print("1. Djibouti - 38 Cities.")
-        print("2. Qatar - 194 Cities.\n")
-        choice = int(input("Your choice? "))
-
         """
         Traveling salesman problem: is, given a set of cities, to find the shortest 
         path to visit all of the cities exactly once.
         """
-        parser = argparse.ArgumentParser(description="Metaheuristics")
 
-        fileList = ["data/dj38_tsp.txt", "data/qa194_tsp.txt"]
-        parser.add_argument("--filename",  # name or flags - Either a name or a
-                            # list of option strings
-                            choices=fileList,  # choices - A container of the allowable
-                            # values for the argument.
-                            default=fileList[choice - 1],  # default - The value produced if the
-                            # argument is absent from the command line.
-                            help='A string represents a dataset'  # help - A brief
-                            # description of what the argument does.
-                            )
+        while True:
+            print()
+            print("\n Choose an option:")
+            print("1: Djibouti - 38 Cities.")
+            print("2: Qatar - 194 Cities.")
+            print("Press any other KEY to EXIT!")
+            choice = int(input("\n Your choice? "))
 
-        args = parser.parse_args()
+            if choice == 1:
+                filename = "data/dj38_tsp.txt"
+            elif choice == 2:
+                filename = "data/qa194_tsp.txt"
+            else:
+                print("\n EXIT ...")
+                break
 
-        # load and read data ----------------------------------------------------
-        print("..." * 15)
-        _Nodes = coordinatesFile(args.filename)
-        cityName = []
-        latitude = []
-        longitude = []
+                # load and read data ----------------------------------------------------
+            print("..." * 15)
+            _Nodes = coordinatesFile(filename)
+            cityName = []
+            latitude = []
+            longitude = []
 
-        solutionObj, fitnessObj = runGenetic(_Nodes)
+            solutionObj, fitnessObj = runGenetic(_Nodes)
 
-        # Prepare the data for plotting the trip
-        for city in solutionObj.trip:
-            # print(city.latitude)
-            cityName.append(city.cityName)
-            latitude.append(city.latitude / 1000)
-            longitude.append(city.longitude / 1000)
-        cityName.append(cityName[0])
-        latitude.append(latitude[0])
-        longitude.append(longitude[0])
-        # print(longitude)
+            for city in solutionObj.trip:
+                # print(city.latitude)
+                cityName.append(city.cityName)
+                latitude.append(city.latitude / 1000)
+                longitude.append(city.longitude / 1000)
+            cityName.append(cityName[0])
+            latitude.append(latitude[0])
+            longitude.append(longitude[0])
+            # print(longitude)
 
-        # Visualize
-        if choice == 1:
-            plotSA(cityName, latitude, longitude, "Djibouti")
-        else:
-            plotSA(cityName, latitude, longitude, "Qatar")
+            # Visualize
+            if choice == 1:
+                plotSA(cityName, latitude, longitude, "Djibouti")
+            else:
+                plotSA(cityName, latitude, longitude, "Qatar")
 
-        plot_learning(fitnessObj)
+            plot_learning(fitnessObj)
+
     except:
         EH()
 
@@ -133,7 +120,20 @@ def main():
 if __name__ == "__main__":
     try:
         # Install Required packages
+        from setup import install_required_Packages
+
         install_required_Packages()
+
+        import datetime as dt
+
+        from GAAlgorithm import GeneticAlgorithm as GA \
+            , Population \
+            , TripManager \
+            , Node
+
+        from eHandler import PrintException as EH
+        from plotTSP import plotSA, plot_learning
+
         main()
 
     except:
